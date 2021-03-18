337230ee1adcabd8e7886887ed5e6d50f89862c3
Zontex <rongo1706@Gmail.com>
2021-03-18 14:53:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'edupo
 from eduponics import __version__
 sys.path.pop(0)
 
+# read the contents of your README file
+this_directory = os.path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='micropython-eduponics',
     packages=find_packages(),
@@ -17,8 +22,9 @@ setup(
     version=__version__,
     download_url = 'https://github.com/STEMinds/micropython-eduponics/archive/1.0.7.tar.gz',
     keywords = ["STEMinds",'MicroPython','uPython', 'Eduponics-Mini', 'Eduponics', 'ESP32', 'ADS1x15', 'MCP23017', 'TDS', 'pH', 'bh1750', 'BME280', 'DS1307', 'AT24C02'],
-    description='',
-    long_description='',
+    description='STEMinds Eduponics Mini MicroPython library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license="MIT",
     classifiers=[
         'Development Status :: 4 - Beta',
